5026d5560412baabce8ee1bc251efce3a6fb92af
Nicole Vafiadis <nicoleh.vafiadis@gmail.com>
2026-07-27 11:33:58 +0900

    Functional Plink flippers & main menu bg

---
+++ Gotcha Game/Assets/Scripts/Level/PlinkoLauncher.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlinkoLauncher : MonoBehaviour
+{
+    [SerializeField] private InputActionReference jumpRef;
+    public float launchForce = 10f;
+    public Transform spriteTransform;
+    public Vector3 centerPoint;
+    private float previousAngle = 0f;
+
+    void Start()
+    {
+        centerPoint = transform.GetChild(0).transform.position;
+        spriteTransform = transform;
+    }
+
+    private void OnEnable()
+    {
+        jumpRef.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        jumpRef.action.Disable();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (jumpRef.action.triggered)
+            {
+                StartCoroutine(LaunchPlinko());
+                Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+                if (rb)
+                {
+                    rb.linearVelocity = Vector2.zero;
+                    rb.AddForce(transform.up * launchForce, ForceMode2D.Impulse);
+                }
+            }
+        }
+    }
+
+    private IEnumerator LaunchPlinko()
+    {
+        float duration = 0.5f;
+
+        DOVirtual.Float(0f, -15f, duration, onVirtualUpdate: (float currentAngle) =>
+            {
+                float deltaAngle = currentAngle - previousAngle;
+                spriteTransform.RotateAround(centerPoint, Vector3.forward, deltaAngle);
+                previousAngle = currentAngle;
+            }).SetEase(Ease.OutQuad);
+
+        yield return new WaitForSeconds(1.5f);
+
+        DOVirtual.Float(-15f, 0f, duration, onVirtualUpdate: (float currentAngle) =>
+            {
+                float deltaAngle = currentAngle - previousAngle;
+                spriteTransform.RotateAround(centerPoint, Vector3.forward, deltaAngle);
+                previousAngle = currentAngle;
+            }).SetEase(Ease.OutQuad);
+    }
+}
